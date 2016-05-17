ffd97d43ebe7f0c7b888d6f06cc6e8d581144b93
M-DOL <mjdolan@umich.edu>
2016-05-17 10:29:31 -0400

    DB

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health, test
+from mcube.views import index, health, test
 
 urlpatterns = [
     # Examples:
